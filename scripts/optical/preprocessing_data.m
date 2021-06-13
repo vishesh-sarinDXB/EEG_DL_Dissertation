b9172fed565bfd1349142bc3f6ea4ffff3a4a4cc
Sarin, Vishesh <vs62@hw.ac.uk>
2021-06-13 16:12:54 +0530

    Amended preprocessing script so all movement specific data saved in different structs

--- scripts/optical/preprocessing_data.m
+++ scripts/optical/preprocessing_data.m
@@ -2,9 +2,18 @@ data_dir = '../../data/mat_data/';
 filePattern = fullfile(data_dir, '*.mat');
 files = dir(filePattern);
 eeg_all = [];
+real_left = [];
+real_right = [];
+mi_left = [];
+mi_right = [];
+
 
 for k = 1 : length(files)
     fullFileName = fullfile(files(k).folder, files(k).name);
     load(fullFileName);
     eeg_all = [eeg_all;eeg]; %#ok<AGROW>
+    real_left = [real_left ; eeg.movement_left]; %#ok<AGROW>
+    real_right = [real_right ; eeg.movement_right]; %#ok<AGROW>
+    mi_left = [mi_left ; eeg.imagery_left]; %#ok<AGROW>
+    mi_right = [mi_right ; eeg.imagery_right]; %#ok<AGROW>
 end
