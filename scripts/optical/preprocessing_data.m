77b3e9233d6ebbfc07e4a842ee0072295b586bce
Sarin, Vishesh <vs62@hw.ac.uk>
2021-06-13 15:11:53 +0530

    Looping now saves every subjects eeg data in a single struct

--- scripts/optical/preprocessing_data.m
+++ scripts/optical/preprocessing_data.m
@@ -1,9 +1,10 @@
 data_dir = '../../data/mat_data/';
 filePattern = fullfile(data_dir, '*.mat');
 files = dir(filePattern);
+eeg_all = [];
 
-for k = 1: length(files)
+for k = 1 : length(files)
     fullFileName = fullfile(files(k).folder, files(k).name);
-%     load(fullFileName)
+    load(fullFileName);
+    eeg_all = [eeg_all;eeg]; %#ok<AGROW>
 end
-
