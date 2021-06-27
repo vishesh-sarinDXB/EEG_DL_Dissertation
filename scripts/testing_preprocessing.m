072b88b159b1ad4079ab47232c3f0ecef2f4ce2f
Sarin, Vishesh <vs62@hw.ac.uk>
2021-06-27 17:11:32 +0530

    Added code to testing_preprocessing to loop through all subjects in data/raw/mat_data

--- scripts/testing_preprocessing.m
+++ scripts/testing_preprocessing.m
@@ -1,62 +1,82 @@
-load('../data/raw/mat_data/s01.mat');
+% load('../data/raw/mat_data/s01.mat');
 
-% load('./miscellaneous/BCICIV_calib_ds1a.mat');
+data_dir = '../../data/raw/mat_data/';
+filePattern = fullfile(data_dir, '*.mat');
+data_dir = dir(filePattern);
 
-fs = eeg.srate;
+if ~exist('../data/processed/', 'dir')
+    mkdir ../data/processed/
+end
+
+data_processed_dir = dir('../data/processed/');
+
 startEpoch = 0.5;
 endEpoch = 2.5;
 
-nbSamplesPerTrial = ceil((endEpoch - startEpoch) * fs);
-
 nbChannels = 64;
 
-cues_real = find(eeg.movement_event == 1);
-cues_mi = find(eeg.imagery_event == 1);
+for k = 1 : length(data_dir)
+    
+    fullFileName = fullfile(data_dir(k).folder, data_dir(k).name);
+    load(fullFileName);
 
-nbTrials_real = length(cues_real);
-nbTrials_mi = length(cues_mi);
+    fs = eeg.srate;
 
-real = zeros(nbChannels, nbSamplesPerTrial, (nbTrials_real*2));
-mi = zeros(nbChannels, nbSamplesPerTrial, (nbTrials_mi*2));
+    nbSamplesPerTrial = ceil((endEpoch - startEpoch) * fs);
 
-[B,A] = butter(4,[7 30]/(fs/2));   % [8 30]
+    cues_real = find(eeg.movement_event == 1);
+    cues_mi = find(eeg.imagery_event == 1);
 
-movement_left = eeg.movement_left((1:64), :);
-movement_right = eeg.movement_right((1:64), :);
+    nbTrials_real = length(cues_real);
+    nbTrials_mi = length(cues_mi);
 
-for trial = 1 : nbTrials_real
-    cueIndex = cues_real(trial);
-    epoch = movement_left(:, (cueIndex + round(startEpoch*fs)):(cueIndex + round(endEpoch*fs))-1);
-    epoch = filter(B,A,epoch);
-    real(:,:,trial) = epoch;
+    real = zeros(nbChannels, nbSamplesPerTrial, (nbTrials_real*2));
+    mi = zeros(nbChannels, nbSamplesPerTrial, (nbTrials_mi*2));
+
+    [B,A] = butter(4,[7 30]/(fs/2));   % [8 30]
+
+    movement_left = eeg.movement_left((1:64), :);
+    movement_right = eeg.movement_right((1:64), :);
+
+    for trial = 1 : nbTrials_real
+        cueIndex = cues_real(trial);
+        epoch = movement_left(:, (cueIndex + round(startEpoch*fs)):(cueIndex + round(endEpoch*fs))-1);
+        epoch = filter(B,A,epoch);
+        real(:,:,trial) = epoch;
+    end
+
+    for trial = (nbTrials_real + 1) : (nbTrials_real * 2)
+        cueIndex = cues_real(trial - nbTrials_real);
+        epoch = movement_right(:, (cueIndex + round(startEpoch*fs)):(cueIndex + round(endEpoch*fs))-1);
+        epoch = filter(B,A,epoch);
+        real(:,:,trial) = epoch;
+    end
+
+    class_real(1:nbTrials_real) = 1;
+    class_real((nbTrials_real + 1) : (nbTrials_real * 2)) = 2;
+
+    mi_left = eeg.imagery_left((1:64), :);
+    mi_right = eeg.imagery_right((1:64), :);
+
+    for trial = 1 : nbTrials_mi
+        cueIndex = cues_mi(trial);
+        epoch = mi_left(:, (cueIndex + round(startEpoch*fs)):(cueIndex + round(endEpoch*fs))-1);
+        epoch = filter(B,A,epoch);
+        mi(:,:,trial) = epoch;
+    end
+
+    for trial = (nbTrials_mi + 1) : (nbTrials_mi * 2)
+        cueIndex = cues_mi(trial - nbTrials_mi);
+        epoch = mi_right(:, (cueIndex + round(startEpoch*fs)):(cueIndex + round(endEpoch*fs))-1);
+        epoch = filter(B,A,epoch);
+        mi(:,:,trial) = epoch;
+    end
+
+    class_mi(1:nbTrials_real) = 1;
+    class_mi((nbTrials_real + 1) : (nbTrials_real * 2)) = 2;
+    
+    fullFileName = fullfile(data_processed_dir(1).folder, data_dir(k).name);
+    
+    save(fullFileName, 'real', 'mi', 'class_mi', 'class_real');
+
 end
-
-for trial = (nbTrials_real + 1) : (nbTrials_real * 2)
-    cueIndex = cues_real(trial - nbTrials_real);
-    epoch = movement_right(:, (cueIndex + round(startEpoch*fs)):(cueIndex + round(endEpoch*fs))-1);
-    epoch = filter(B,A,epoch);
-    real(:,:,trial) = epoch;
-end
-
-class_real(1:nbTrials_real) = 1;
-class_real((nbTrials_real + 1) : (nbTrials_real * 2)) = 2;
-
-mi_left = eeg.imagery_left((1:64), :);
-mi_right = eeg.imagery_right((1:64), :);
-
-for trial = 1 : nbTrials_mi
-    cueIndex = cues_mi(trial);
-    epoch = mi_left(:, (cueIndex + round(startEpoch*fs)):(cueIndex + round(endEpoch*fs))-1);
-    epoch = filter(B,A,epoch);
-    mi(:,:,trial) = epoch;
-end
-
-for trial = (nbTrials_mi + 1) : (nbTrials_mi * 2)
-    cueIndex = cues_mi(trial - nbTrials_mi);
-    epoch = mi_right(:, (cueIndex + round(startEpoch*fs)):(cueIndex + round(endEpoch*fs))-1);
-    epoch = filter(B,A,epoch);
-    mi(:,:,trial) = epoch;
-end
-
-class_mi(1:nbTrials_real) = 1;
-class_mi((nbTrials_real + 1) : (nbTrials_real * 2)) = 2;
