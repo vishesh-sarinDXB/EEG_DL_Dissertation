0eb5c6b2f7c255f007c045d1cff037a4e2747c68
Sarin, Vishesh <vs62@hw.ac.uk>
2021-07-19 15:43:07 +0530

    Added newest experiment call
    Most recent experiment on processed data with bandpass filter between 8 and 14 Hz thus isolating alpha and mu frequencies
    
    All channels are used

--- scripts/optical/Experiments.m
+++ scripts/optical/Experiments.m
@@ -5,3 +5,7 @@ OPTICALexperiment('../../data/processed/sevenThirty/', 'all_channels_experiments
 % parietal lobes. Allowing channels that are in between one of these and
 % other lobes. Using bandpass filter between 7 and 30 Hz
 OPTICALexperiment('../../data/processed/sevenThirty/', 'bio_channels_experiments', [1:14, 17:19, 32:51, 54:56]);
+
+% Using all channels, but bandpass ButterWorth filter between 8 and 14 Hz
+% thus isolating alpha and mu waves
+OPTICALexperiment('../../data/processed/eightFourteen/', 'all_channels_alphamu_experiments', [1:64])
