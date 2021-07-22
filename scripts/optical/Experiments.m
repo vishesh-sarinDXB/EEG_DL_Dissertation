1ba3340a307c671eebfbb8fac0299673510e1f41
Sarin, Vishesh <vs62@hw.ac.uk>
2021-07-22 13:25:05 +0530

    Added new Experiments to script
    Added new experiments isolating alpha wave from Occipital Lobes and mu waves from Motor cortex to Experiments.m

--- scripts/optical/Experiments.m
+++ scripts/optical/Experiments.m
@@ -21,7 +21,7 @@ clear
 % parietal lobes. Allowing channels that are in between one of these and
 % other lobes. Using bandpass filter between 8 and 14 Hz thus isolating 
 % alpha and mu waves
-OPTICALexperiment('../../data/processed/eightFourteen/', 'bio_channelsOPT_alphamu', [1:14, 17:19, 32:51, 54:56]); %new
+OPTICALexperiment('../../data/processed/eightFourteen/', 'bio_channelsOPT_alphamu', [1:14, 17:19, 32:51, 54:56]);
 clear
 
 % Only using channels that aren't related to temporal, or
@@ -34,5 +34,17 @@ clear
 % parietal lobes. Allowing channels that are in between one of these and
 % other lobes. Using bandpass filter between 8 and 14 Hz thus isolating
 % alpha and mu waves
-OPTICALexperiment('../../data/processed/eightFourteen/', 'bio_channelsPT_alphamu', [1:14, 17:19, 25:30, 32:51, 54:56, 62:64]); %new
+OPTICALexperiment('../../data/processed/eightFourteen/', 'bio_channelsPT_alphamu', [1:14, 17:19, 25:30, 32:51, 54:56, 62:64]);
+clear
+
+% Only using channels related to occipital lobe. 
+% Allowing channels that are in between occipital and other lobes.
+% Using bandpass filter between 8 and 14 Hz thus isolating alpha waves
+OPTICALexperiment('../../data/processed/eightFourteen/', 'bio_channelsOonly_alphamu', [25:30, 62:64]);
+clear
+
+% Only using channels related to motor cortex (C electrodes). 
+% Allowing channels that are in between C electrodes and other regions.
+% Using bandpass filter between 8 and 14 Hz thus isolating mu waves
+OPTICALexperiment('../../data/processed/eightFourteen/', 'bio_channelsConly_alphamu', [9:14, 17:19, 32, 44:51, 54:56]);
 clear
