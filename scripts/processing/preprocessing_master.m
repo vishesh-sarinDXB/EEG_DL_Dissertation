ec435228baae3ca6e71802a7de484b1c34c9e407
Sarin, Vishesh <vs62@hw.ac.uk>
2021-09-12 13:17:17 +0530

    Changed preprocessing_master epochs
    Continued experimentation, startEpoch now 2.5, end 4.5

--- scripts/processing/preprocessing_master.m
+++ scripts/processing/preprocessing_master.m
@@ -3,7 +3,7 @@ raw_data = '/home/swagmaster/EEG_DL_Dissertation/ftp.cngb.org/pub/gigadb/pub/10.
 % All channels, start epoch 0.5 seconds after cue (2.5 seconds after start
 % of trial) and epoch lasts 2 seconds. Fourth order, band pass butterworth
 % filter, between 7 and 30 Hz. 
-preprocessing_gigaDB(raw_data, 'sevenThirty', 0.5, 2.5, 64, 1, 7, 30)
+preprocessing_gigaDB(raw_data, 'sevenThirty', 2.5, 4.5, 64, 1, 7, 30)
 
 % All channels, start epoch 0.5 seconds after cue (2.5 seconds after start
 % of trial) and epoch lasts 2 seconds. Fourth order, band pass butterworth
