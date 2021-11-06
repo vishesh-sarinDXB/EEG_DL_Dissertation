e7845e16e03858c28b792ede005eab9732cfab67
Sarin, Vishesh <vs62@hw.ac.uk>
2021-11-06 14:10:42 +0400

    Mid-run commit and push

--- scripts/optical/ExperimentsCV.m
+++ scripts/optical/ExperimentsCV.m
@@ -1,29 +1,29 @@
-% Experiment names will describe channels used first, then frequencies. If
-% frequencies are ommited then a bandpass filter between 7 and 30 Hz maybe
-% assumed.
-
-% All channels, only basic bandpass filter between 7 and 30 Hz
-
-OPTICALexperimentCVcaller('../../data/processed/sevenThirty/', 'all', [1:64]);
-clear
-
-% Using all channels, but bandpass ButterWorth filter between 8 and 14 Hz
-% thus isolating alpha and mu waves
-OPTICALexperimentCVcaller('../../data/processed/eightFourteen/', 'all_channels_alphamu', [1:64]);
-clear
-
-% Only using channels that aren't related to occipital, temporal, or
-% parietal lobes. Allowing channels that are in between one of these and
-% other lobes. Using bandpass filter between 7 and 30 Hz
-OPTICALexperimentCVcaller('../../data/processed/sevenThirty/', 'bio_channelsOPT', [1:14, 17:19, 32:51, 54:56]);
-clear
-
-% Only using channels that aren't related to , occipital, temporal, or
-% parietal lobes. Allowing channels that are in between one of these and
-% other lobes. Using bandpass filter between 8 and 14 Hz thus isolating 
-% alpha and mu waves
-OPTICALexperimentCVcaller('../../data/processed/eightFourteen/', 'bio_channelsOPT_alphamu', [1:14, 17:19, 32:51, 54:56]);
-clear
+% % Experiment names will describe channels used first, then frequencies. If
+% % frequencies are ommited then a bandpass filter between 7 and 30 Hz maybe
+% % assumed.
+% 
+% % All channels, only basic bandpass filter between 7 and 30 Hz
+% 
+% OPTICALexperimentCVcaller('../../data/processed/sevenThirty/', 'all', [1:64]);
+% clear
+% 
+% % Using all channels, but bandpass ButterWorth filter between 8 and 14 Hz
+% % thus isolating alpha and mu waves
+% OPTICALexperimentCVcaller('../../data/processed/eightFourteen/', 'all_channels_alphamu', [1:64]);
+% clear
+% 
+% % Only using channels that aren't related to occipital, temporal, or
+% % parietal lobes. Allowing channels that are in between one of these and
+% % other lobes. Using bandpass filter between 7 and 30 Hz
+% OPTICALexperimentCVcaller('../../data/processed/sevenThirty/', 'bio_channelsOPT', [1:14, 17:19, 32:51, 54:56]);
+% clear
+% 
+% % Only using channels that aren't related to , occipital, temporal, or
+% % parietal lobes. Allowing channels that are in between one of these and
+% % other lobes. Using bandpass filter between 8 and 14 Hz thus isolating 
+% % alpha and mu waves
+% OPTICALexperimentCVcaller('../../data/processed/eightFourteen/', 'bio_channelsOPT_alphamu', [1:14, 17:19, 32:51, 54:56]);
+% clear
 
 % Only using channels that aren't related to temporal, or
 % parietal lobes. Allowing channels that are in between one of these and
